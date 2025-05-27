2203c69823a37cd495662555897f7531afa15aa1
boxizhang <bossyzhang@gmail.com>
2025-05-27 12:58:32 +0200

    update

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,13 +1,13 @@
-test_that("hello cicdworkshop greets the attendees", {
+test_that("hello cicdworkshop", {
   expect_message(
     hello(),
-    "^Welcome to the R/Pharma CI/CD workshop!\\n"
+    "^Welcome to the CI/CD workshop!\\n"
   )
 })
 
 test_that("linter_ex is surprised", {
   expect_message(
     linter_ex("lint"),
-    "^Whoa!\\n"
+    "^BingGo!\\n"
   )
 })
