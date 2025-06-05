4385c3458e0d907e75318cfa0e1bdfca98a2c1d1
boxizhang <bossyzhang@gmail.com>
2025-06-05 15:49:03 +0200

    update

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,15 +1,15 @@
 test_that("hello cicdworkshop", {
   expect_message(
     olink(),
-    "^Welcome to the CI/CD workshop!\\n"
-    #"^We have great products and great people!!!!!\\n"
+    #"^Welcome to the CI/CD workshop!\\n"
+    "^We have great products and great people!!!!!\\n"
   )
 })
 
 test_that("linter_ex is surprised", {
   expect_message(
     linter_ex("lint"),
-    #"^BingGo!\\n"
-    "^Whoa!!\\n"
+    "^BingGo!\\n"
+    #"^Whoa!!\\n"
   )
 })
